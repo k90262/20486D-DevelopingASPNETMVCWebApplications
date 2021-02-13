f142d78c641a6dceef499b8a0c2682d0610b86ec
Bill Ho <ycho.bill.lab@gmail.com>
2021-02-13 17:45:41 +0800

    finish training: Module 1: Exploring ASP.NET Core MVC Exercise 1: Exploring a Razor Pages Application

---
+++ Allfiles/Mod01/Labfiles/01_ActorsRazorPages_begin/ActorsRazorPages/ActorsRazorPages/Models/IData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ActorsRazorPages.Models
+{
+    public interface IData
+    {
+        List<Actor> ActorsList { get; set; }
+        List<Actor> ActorsInitializeData();
+        Actor GetActorById(int? id);
+    }
+}
